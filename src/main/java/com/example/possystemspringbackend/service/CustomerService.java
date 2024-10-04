b3655ddb15a0fdf45aeef79cf31b12a296c26b4d
LRLayan <rameshlayan4@gmail.com>
2024-10-04 16:36:22 +0530

    CustomerService

--- src/main/java/com/example/possystemspringbackend/service/CustomerService.java
+++ src/main/java/com/example/possystemspringbackend/service/CustomerService.java
@@ -1,4 +1,13 @@
 package com.example.possystemspringbackend.service;
 
+import com.example.possystemspringbackend.dto.impl.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDTO customerDTO);
+    void updateCustomer(String customerId,CustomerDTO customerDTO);
+    void deleteCustomer(String customerId);
+    CustomerDTO getCustomer(String customerId);
+    List<CustomerDTO>getAllCustomer();
 }
--- src/main/java/com/example/possystemspringbackend/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/possystemspringbackend/service/impl/CustomerServiceImpl.java
@@ -1,10 +1,37 @@
 package com.example.possystemspringbackend.service.impl;
 
+import com.example.possystemspringbackend.dto.impl.CustomerDTO;
 import com.example.possystemspringbackend.service.CustomerService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void updateCustomer(String customerId, CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String customerId) {
+
+    }
+
+    @Override
+    public CustomerDTO getCustomer(String customerId) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomer() {
+        return null;
+    }
 }
