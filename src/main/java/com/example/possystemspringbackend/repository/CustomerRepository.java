24d292baf524d81dde2523081a7c5ff9f59bdeb4
LRLayan <rameshlayan4@gmail.com>
2024-10-04 16:11:23 +0530

    CustomerRepository

---
+++ src/main/java/com/example/possystemspringbackend/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.possystemspringbackend.repository;
+
+import com.example.possystemspringbackend.entity.impl.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity,String> {
+}
