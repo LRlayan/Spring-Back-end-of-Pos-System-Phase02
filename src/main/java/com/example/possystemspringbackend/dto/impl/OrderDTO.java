917c41b7c9b745e73e6713d77aa7686613cca68f
LRLayan <rameshlayan4@gmail.com>
2024-10-04 16:06:30 +0530

    add ID

--- src/main/java/com/example/possystemspringbackend/dto/impl/OrderDTO.java
+++ src/main/java/com/example/possystemspringbackend/dto/impl/OrderDTO.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Data
 public class OrderDTO implements SuperDTO {
+    private String id;
     private String orderID;
     private LocalDate date;
     private String cusId;
