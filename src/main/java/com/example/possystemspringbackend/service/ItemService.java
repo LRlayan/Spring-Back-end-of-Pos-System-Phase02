5b479a717a3ca52c64e8816fe58844beef6a6cac
LRLayan <rameshlayan4@gmail.com>
2024-10-04 16:36:29 +0530

    CustomerService

--- src/main/java/com/example/possystemspringbackend/service/ItemService.java
+++ src/main/java/com/example/possystemspringbackend/service/ItemService.java
@@ -1,4 +1,13 @@
 package com.example.possystemspringbackend.service;
 
+import com.example.possystemspringbackend.dto.impl.CustomerDTO;
+
+import java.util.List;
+
 public interface ItemService {
+    void saveItem(CustomerDTO customerDTO);
+    void updateItem(String customerId,CustomerDTO customerDTO);
+    void deleteItem(String customerId);
+    CustomerDTO getItem(String customerId);
+    List<CustomerDTO> getAllItem();
 }
--- src/main/java/com/example/possystemspringbackend/service/impl/ItemServiceImpl.java
+++ src/main/java/com/example/possystemspringbackend/service/impl/ItemServiceImpl.java
@@ -1,10 +1,37 @@
 package com.example.possystemspringbackend.service.impl;
 
+import com.example.possystemspringbackend.dto.impl.CustomerDTO;
 import com.example.possystemspringbackend.service.ItemService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class ItemServiceImpl implements ItemService {
+    @Override
+    public void saveItem(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void updateItem(String customerId, CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteItem(String customerId) {
+
+    }
+
+    @Override
+    public CustomerDTO getItem(String customerId) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllItem() {
+        return null;
+    }
 }
