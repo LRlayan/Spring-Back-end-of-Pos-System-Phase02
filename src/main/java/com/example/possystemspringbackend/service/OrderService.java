882316578b2be6e88fb9f63b8f935310b782f279
LRLayan <rameshlayan4@gmail.com>
2024-10-04 16:36:56 +0530

    OrderService

--- src/main/java/com/example/possystemspringbackend/service/OrderService.java
+++ src/main/java/com/example/possystemspringbackend/service/OrderService.java
@@ -1,4 +1,14 @@
 package com.example.possystemspringbackend.service;
 
+import com.example.possystemspringbackend.dto.impl.CustomerDTO;
+import com.example.possystemspringbackend.dto.impl.OrderDTO;
+
+import java.util.List;
+
 public interface OrderService {
+    void saveOrder(OrderDTO orderDTO);
+    void updateOrder(String orderId,OrderDTO orderDTO);
+    void deleteOrder(String orderId);
+    OrderDTO getOrder(String orderId);
+    List<OrderDTO> getAllOrder();
 }
--- src/main/java/com/example/possystemspringbackend/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/possystemspringbackend/service/impl/OrderServiceImpl.java
@@ -1,10 +1,38 @@
 package com.example.possystemspringbackend.service.impl;
 
+import com.example.possystemspringbackend.dto.impl.OrderDTO;
 import com.example.possystemspringbackend.service.OrderService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class OrderServiceImpl implements OrderService {
+    @Override
+    public void saveOrder(OrderDTO orderDTO) {
+
+    }
+
+    @Override
+    public void updateOrder(String orderId, OrderDTO orderDTO) {
+
+    }
+
+    @Override
+    public void deleteOrder(String orderId) {
+
+    }
+
+    @Override
+    public OrderDTO getOrder(String orderId) {
+        return null;
+    }
+
+    @Override
+    public List<OrderDTO> getAllOrder() {
+        return null;
+    }
 }
+
