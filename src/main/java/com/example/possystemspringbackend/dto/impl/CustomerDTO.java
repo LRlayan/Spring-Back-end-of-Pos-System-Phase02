0af4d4f9deb4493e747571b567e7a2840033a69b
LRLayan <rameshlayan4@gmail.com>
2024-10-04 15:37:45 +0530

    CustomerDTO

---
+++ src/main/java/com/example/possystemspringbackend/dto/impl/CustomerDTO.java
@@ -0,0 +1,16 @@
+package com.example.possystemspringbackend.dto.impl;
+
+import com.example.possystemspringbackend.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomerDTO implements SuperDTO {
+    private String id;
+    private String name;
+    private String city;
+    private String tel;
+}
