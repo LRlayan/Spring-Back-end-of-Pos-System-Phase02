6a61d967277b30e6c835fb3396e0d9f39d7b12f2
LRLayan <rameshlayan4@gmail.com>
2024-10-04 14:47:58 +0530

    entityManagerFactory()

--- src/main/java/com/example/possystemspringbackend/config/WebAppRootConfig.java
+++ src/main/java/com/example/possystemspringbackend/config/WebAppRootConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -23,5 +25,16 @@ public class WebAppRootConfig {
         driverManagerDataSource.setUsername("root");
         driverManagerDataSource.setPassword("Ijse@1234");
         return driverManagerDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        var vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        var entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactoryBean.setPackagesToScan("com.example.possystemspringbackend.entity");
+        entityManagerFactoryBean.setDataSource(dataSource());
+        return entityManagerFactoryBean;
     }
 }
