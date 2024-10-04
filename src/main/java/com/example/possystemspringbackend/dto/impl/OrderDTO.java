4376df1b28319705561aded99deca897490cd88a
LRLayan <rameshlayan4@gmail.com>
2024-10-04 15:38:20 +0530

    OrderDTO

---
+++ src/main/java/com/example/possystemspringbackend/dto/impl/OrderDTO.java
@@ -0,0 +1,21 @@
+package com.example.possystemspringbackend.dto.impl;
+
+import com.example.possystemspringbackend.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderDTO implements SuperDTO {
+    private String orderID;
+    private LocalDate date;
+    private String cusId;
+    private double discountRate;
+    private double discount;
+    private double subTotal;
+    private double balance;
+}
