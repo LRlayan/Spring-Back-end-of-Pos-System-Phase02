3b7aa7a42a4e218a9e15aa8ceeeff04ad65851a8
LRLayan <rameshlayan4@gmail.com>
2024-10-04 15:38:00 +0530

    ItemDTO

---
+++ src/main/java/com/example/possystemspringbackend/dto/impl/ItemDTO.java
@@ -0,0 +1,16 @@
+package com.example.possystemspringbackend.dto.impl;
+
+import com.example.possystemspringbackend.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemDTO implements SuperDTO {
+    private String itemCode;
+    private String itemName;
+    private int QTYOnHand;
+    private double unitPrice;
+}
