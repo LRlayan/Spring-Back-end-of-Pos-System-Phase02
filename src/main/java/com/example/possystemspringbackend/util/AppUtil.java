3670264f814cdc20e0c01579207c2d083784ac18
LRLayan <rameshlayan4@gmail.com>
2024-10-07 19:14:52 +0530

    method static

--- src/main/java/com/example/possystemspringbackend/util/AppUtil.java
+++ src/main/java/com/example/possystemspringbackend/util/AppUtil.java
@@ -3,16 +3,16 @@ package com.example.possystemspringbackend.util;
 import java.util.UUID;
 
 public class AppUtil {
-    public String generateCustomerId(){
+    public static String generateCustomerId(){
         return "CUSTOMER-" + UUID.randomUUID();
     }
-    public String generateItemId(){
+    public static String generateItemId(){
         return "ITEM-" + UUID.randomUUID();
     }
-    public String generateOrderId(){
+    public static String generateOrderId(){
         return "ORDER-" + UUID.randomUUID();
     }
-    public String generateOrderDetailId(){
+    public static String generateOrderDetailId(){
         return "ORDER_DETAIL-" + UUID.randomUUID();
     }
 }
