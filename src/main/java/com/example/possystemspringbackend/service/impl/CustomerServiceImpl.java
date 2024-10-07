d076fa8939457f6a681752660766ed30c11d0d9f
LRLayan <rameshlayan4@gmail.com>
2024-10-07 19:13:01 +0530

    dependency injection

--- src/main/java/com/example/possystemspringbackend/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/possystemspringbackend/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.possystemspringbackend.service.impl;
 
 import com.example.possystemspringbackend.dto.impl.CustomerDTO;
+import com.example.possystemspringbackend.repository.CustomerRepository;
 import com.example.possystemspringbackend.service.CustomerService;
+import com.example.possystemspringbackend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +13,14 @@ import java.util.List;
 @Service
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private Mapping mapping;
+
     @Override
     public void saveCustomer(CustomerDTO customerDTO) {
-
     }
 
     @Override
