90f818f1e0a48c4cda39a46ef163884a6603fc58
LRLayan <rameshlayan4@gmail.com>
2024-10-07 19:11:08 +0530

    remove

--- src/main/java/com/example/possystemspringbackend/controller/OrderDetailController.java
+++
@@ -1,4 +0,0 @@
-package com.example.possystemspringbackend.controller;
-
-public class OrderDetailController {
-}
--- src/main/java/com/example/possystemspringbackend/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.possystemspringbackend.repository;
-
-import com.example.possystemspringbackend.entity.impl.OrderDetailEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetailEntity,String> {
-}
