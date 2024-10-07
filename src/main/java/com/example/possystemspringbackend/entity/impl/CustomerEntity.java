d199eb7a1908a9869701b08687a6e856704d8a22
LRLayan <rameshlayan4@gmail.com>
2024-10-07 19:13:47 +0530

    relationship

--- src/main/java/com/example/possystemspringbackend/entity/impl/CustomerEntity.java
+++ src/main/java/com/example/possystemspringbackend/entity/impl/CustomerEntity.java
@@ -1,10 +1,7 @@
 package com.example.possystemspringbackend.entity.impl;
 
 import com.example.possystemspringbackend.entity.SuperEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,11 +15,10 @@ import java.util.List;
 @Table(name = "customer")
 public class CustomerEntity implements SuperEntity {
     @Id
-    private String id;
     private String customerId   ;
     private String name;
     private String city;
     private String tel;
-    @OneToMany(mappedBy = "customer")
+    @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
     private List<OrderEntity> orderList;
 }
