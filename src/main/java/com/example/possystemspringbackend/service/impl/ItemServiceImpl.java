6cf4e058ca77458b84caa4acf92f0af7c1903ca2
LRLayan <rameshlayan4@gmail.com>
2024-10-09 00:06:13 +0530

    saveItem()

--- src/main/java/com/example/possystemspringbackend/service/impl/ItemServiceImpl.java
+++ src/main/java/com/example/possystemspringbackend/service/impl/ItemServiceImpl.java
@@ -1,37 +1,52 @@
 package com.example.possystemspringbackend.service.impl;
 
-import com.example.possystemspringbackend.dto.impl.CustomerDTO;
+import com.example.possystemspringbackend.customStatusCode.ErrorStatus;
+import com.example.possystemspringbackend.dto.ItemStatus;
+import com.example.possystemspringbackend.dto.impl.ItemDTO;
+import com.example.possystemspringbackend.entity.impl.ItemEntity;
+import com.example.possystemspringbackend.exception.CustomerNotFoundException;
+import com.example.possystemspringbackend.exception.DataPersistException;
+import com.example.possystemspringbackend.repository.ItemRepository;
 import com.example.possystemspringbackend.service.ItemService;
+import com.example.possystemspringbackend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class ItemServiceImpl implements ItemService {
-    @Override
-    public void saveItem(CustomerDTO customerDTO) {
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private Mapping mapping;
 
+    @Override
+    public void saveItem(ItemDTO itemDTO) {
+        ItemEntity item = itemRepository.save(mapping.toItemEntity(itemDTO));
+        if (item==null){
+            throw new DataPersistException("Customer Note Saved");
+        }
     }
 
     @Override
-    public void updateItem(String customerId, CustomerDTO customerDTO) {
-
+    public void updateItem(String itemCode, ItemDTO itemDTO) {
     }
 
     @Override
-    public void deleteItem(String customerId) {
-
+    public void deleteItem(String itemCode) {
     }
 
     @Override
-    public CustomerDTO getItem(String customerId) {
+    public ItemStatus getItem(String itemCode) {
         return null;
     }
 
     @Override
-    public List<CustomerDTO> getAllItem() {
+    public List<ItemDTO> getAllItem() {
         return null;
     }
 }
