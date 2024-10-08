4f48af8573faa381afac1a9489d6ddba185ea47b
LRLayan <rameshlayan4@gmail.com>
2024-10-09 00:08:29 +0530

    add property and implement ItemStatus

--- src/main/java/com/example/possystemspringbackend/dto/impl/ItemDTO.java
+++ src/main/java/com/example/possystemspringbackend/dto/impl/ItemDTO.java
@@ -1,17 +1,20 @@
 package com.example.possystemspringbackend.dto.impl;
 
+import com.example.possystemspringbackend.dto.ItemStatus;
 import com.example.possystemspringbackend.dto.SuperDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class ItemDTO implements SuperDTO {
-    private String id;
+public class ItemDTO implements SuperDTO , ItemStatus {
     private String itemCode;
     private String itemName;
     private int QTYOnHand;
     private double unitPrice;
+    private List<OrderDetailDTO> orderList;
 }
