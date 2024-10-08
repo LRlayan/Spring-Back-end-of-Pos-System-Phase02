ab52bee453ae597ad96631d11dc91de90c279213
LRLayan <rameshlayan4@gmail.com>
2024-10-08 16:37:50 +0530

    Regex

---
+++ src/main/java/com/example/possystemspringbackend/util/Regex.java
@@ -0,0 +1,14 @@
+package com.example.possystemspringbackend.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    public static Matcher customerIdValidate(String customerId){
+        return Pattern.compile("^C\\d{2}-\\d{3,}$").matcher(customerId);
+    }
+
+    public static Matcher itemCodeValidate(String itemCode){
+        return Pattern.compile("^I\\d{2}-\\d{3,}$").matcher(itemCode);
+    }
+}
