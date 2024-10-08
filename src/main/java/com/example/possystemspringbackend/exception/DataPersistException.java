3b087b390cf0b5981f082c29a366072e4758a087
LRLayan <rameshlayan4@gmail.com>
2024-10-08 13:41:32 +0530

    DataPersistException

---
+++ src/main/java/com/example/possystemspringbackend/exception/DataPersistException.java
@@ -0,0 +1,12 @@
+package com.example.possystemspringbackend.exception;
+
+public class DataPersistException extends RuntimeException{
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+    }
+
+    public DataPersistException(String message, Throwable cause) {
+    }
+}
