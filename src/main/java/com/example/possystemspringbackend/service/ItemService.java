c3dea8fca0b4954f700daf0a96ff381635578bff
LRLayan <rameshlayan4@gmail.com>
2024-10-09 00:05:13 +0530

    refactor methods

--- src/main/java/com/example/possystemspringbackend/service/ItemService.java
+++ src/main/java/com/example/possystemspringbackend/service/ItemService.java
@@ -1,13 +1,14 @@
 package com.example.possystemspringbackend.service;
 
-import com.example.possystemspringbackend.dto.impl.CustomerDTO;
+import com.example.possystemspringbackend.dto.ItemStatus;
+import com.example.possystemspringbackend.dto.impl.ItemDTO;
 
 import java.util.List;
 
 public interface ItemService {
-    void saveItem(CustomerDTO customerDTO);
-    void updateItem(String customerId,CustomerDTO customerDTO);
-    void deleteItem(String customerId);
-    CustomerDTO getItem(String customerId);
-    List<CustomerDTO> getAllItem();
+    void saveItem(ItemDTO itemDTO);
+    void updateItem(String itemId,ItemDTO itemDTO);
+    void deleteItem(String itemId);
+    ItemStatus getItem(String itemId);
+    List<ItemDTO> getAllItem();
 }
