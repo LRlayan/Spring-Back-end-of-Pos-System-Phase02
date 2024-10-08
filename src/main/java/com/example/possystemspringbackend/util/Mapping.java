aa0744a0e59494a9888014d254ee6e3ad412b992
LRLayan <rameshlayan4@gmail.com>
2024-10-08 22:01:02 +0530

    customerList()

--- src/main/java/com/example/possystemspringbackend/util/Mapping.java
+++ src/main/java/com/example/possystemspringbackend/util/Mapping.java
@@ -3,8 +3,11 @@ package com.example.possystemspringbackend.util;
 import com.example.possystemspringbackend.dto.impl.CustomerDTO;
 import com.example.possystemspringbackend.entity.impl.CustomerEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Mapping {
@@ -15,7 +18,11 @@ public class Mapping {
         return mapper.map(customerDTO,CustomerEntity.class); //map() -  meken karanne userDto ek UserEntity ekakta convert krnwa
     }
 
-    public CustomerDTO toCustomerDTO(CustomerEntity userentity){
-        return mapper.map(userentity,CustomerDTO.class);
+    public CustomerDTO toCustomerDTO(CustomerEntity userEntity){
+        return mapper.map(userEntity,CustomerDTO.class);
+    }
+
+    public List<CustomerDTO> customerList(List<CustomerEntity> customerList) {
+        return mapper.map(customerList,new TypeToken<List<CustomerDTO>>(){}.getType());
     }
 }
