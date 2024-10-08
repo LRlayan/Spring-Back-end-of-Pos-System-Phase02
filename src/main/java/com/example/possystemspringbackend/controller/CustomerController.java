d050bcbd7219e9ce4e2d44c0448982f28bdf3ffa
LRLayan <rameshlayan4@gmail.com>
2024-10-08 13:40:30 +0530

    add media type and setCustomerId

--- src/main/java/com/example/possystemspringbackend/controller/CustomerController.java
+++ src/main/java/com/example/possystemspringbackend/controller/CustomerController.java
@@ -3,11 +3,12 @@ package com.example.possystemspringbackend.controller;
 import com.example.possystemspringbackend.dto.impl.CustomerDTO;
 import com.example.possystemspringbackend.exception.DataPersistException;
 import com.example.possystemspringbackend.service.CustomerService;
-import com.example.possystemspringbackend.util.AppUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +19,9 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @PostMapping
-    public ResponseEntity<Void> saveCustomer(CustomerDTO customerDTO){
-        customerDTO.setCustomerId(AppUtil.generateCustomerId());
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveCustomer(@RequestBody CustomerDTO customerDTO){
+        customerDTO.setCustomerId(customerDTO.getCustomerId());
         try{
             customerService.saveCustomer(customerDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
