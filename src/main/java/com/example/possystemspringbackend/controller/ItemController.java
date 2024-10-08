06d8ad36f40b30f5d452c90a50aecc87c6b11e33
LRLayan <rameshlayan4@gmail.com>
2024-10-09 00:04:02 +0530

    saveItem()

--- src/main/java/com/example/possystemspringbackend/controller/ItemController.java
+++ src/main/java/com/example/possystemspringbackend/controller/ItemController.java
@@ -1,4 +1,37 @@
 package com.example.possystemspringbackend.controller;
 
+import com.example.possystemspringbackend.customStatusCode.ErrorStatus;
+import com.example.possystemspringbackend.dto.CustomerStatus;
+import com.example.possystemspringbackend.dto.ItemStatus;
+import com.example.possystemspringbackend.dto.impl.ItemDTO;
+import com.example.possystemspringbackend.exception.DataPersistException;
+import com.example.possystemspringbackend.service.ItemService;
+import com.example.possystemspringbackend.util.Regex;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/items")
 public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveItem(@RequestBody ItemDTO itemDTO){
+        itemDTO.setItemCode(itemDTO.getItemCode());
+        try{
+            itemService.saveItem(itemDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
