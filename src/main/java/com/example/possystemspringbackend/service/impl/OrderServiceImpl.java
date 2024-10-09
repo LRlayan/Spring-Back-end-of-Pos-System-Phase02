545704c2190111564c3056b0b5e402767c6d3c2e
LRLayan <rameshlayan4@gmail.com>
2024-10-09 14:06:58 +0530

    saveOrder()

--- src/main/java/com/example/possystemspringbackend/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/possystemspringbackend/service/impl/OrderServiceImpl.java
@@ -1,7 +1,12 @@
 package com.example.possystemspringbackend.service.impl;
 
 import com.example.possystemspringbackend.dto.impl.OrderDTO;
+import com.example.possystemspringbackend.entity.impl.OrderEntity;
+import com.example.possystemspringbackend.exception.DataPersistException;
+import com.example.possystemspringbackend.repository.OrderRepository;
 import com.example.possystemspringbackend.service.OrderService;
+import com.example.possystemspringbackend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +15,16 @@ import java.util.List;
 @Service
 @Transactional
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private Mapping mapping;
     @Override
     public void saveOrder(OrderDTO orderDTO) {
-
+        OrderEntity order = orderRepository.save(mapping.toOrderEntity(orderDTO));
+        if (order==null){
+            throw new DataPersistException("Order Note Saved");
+        }
     }
 
     @Override
