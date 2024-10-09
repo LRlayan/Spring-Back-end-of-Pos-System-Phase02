b7957edf2d9b07b42ee58983c5bc074d606f108f
LRLayan <rameshlayan4@gmail.com>
2024-10-09 14:06:04 +0530

    saveOrder()

--- src/main/java/com/example/possystemspringbackend/controller/OrderController.java
+++ src/main/java/com/example/possystemspringbackend/controller/OrderController.java
@@ -1,4 +1,35 @@
 package com.example.possystemspringbackend.controller;
 
+import com.example.possystemspringbackend.dto.impl.CustomerDTO;
+import com.example.possystemspringbackend.dto.impl.OrderDTO;
+import com.example.possystemspringbackend.exception.DataPersistException;
+import com.example.possystemspringbackend.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/orders")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveOrder(@RequestBody OrderDTO orderDTO){
+        orderDTO.setOrderID(orderDTO.getOrderID());
+        try{
+            orderService.saveOrder(orderDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
