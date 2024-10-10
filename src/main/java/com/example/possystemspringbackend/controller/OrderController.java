75562685dae01319334d7d7a5d851ec85cbbb3f7
LRLayan <rameshlayan4@gmail.com>
2024-10-10 22:53:02 +0530

    Resolve crossOrigin policy and getAllOrders()

--- src/main/java/com/example/possystemspringbackend/controller/OrderController.java
+++ src/main/java/com/example/possystemspringbackend/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.example.possystemspringbackend.controller;
 
-import com.example.possystemspringbackend.dto.impl.CustomerDTO;
 import com.example.possystemspringbackend.dto.impl.OrderDTO;
 import com.example.possystemspringbackend.exception.DataPersistException;
 import com.example.possystemspringbackend.service.OrderService;
@@ -8,13 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/orders")
+@CrossOrigin
 public class OrderController {
     @Autowired
     private OrderService orderService;
@@ -31,5 +30,10 @@ public class OrderController {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<OrderDTO> getAllOrders(){
+        return orderService.getAllOrder();
     }
 }
