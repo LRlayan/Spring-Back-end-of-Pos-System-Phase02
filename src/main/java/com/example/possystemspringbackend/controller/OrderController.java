50b8581d63b8c0ad5d9707a4ab5997709be0f95b
LRLayan <rameshlayan4@gmail.com>
2024-10-13 21:44:14 +0530

    updateOrder()

--- src/main/java/com/example/possystemspringbackend/controller/OrderController.java
+++ src/main/java/com/example/possystemspringbackend/controller/OrderController.java
@@ -35,6 +35,18 @@ public class OrderController {
         }
     }
 
+    @PutMapping(value = "/{orderId}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateOrder(@PathVariable("orderId") String orderId,@RequestBody OrderDTO orderDTO){
+        try{
+            orderService.updateOrder(orderId,orderDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @GetMapping(value = "/{orderId}")
     public OrderStatus getOrder(@PathVariable("orderId") String orderId){
         if (!Regex.orderIdValidate(orderId).matches()){
